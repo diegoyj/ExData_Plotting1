61f3664998e427b47e5431ad8e0e3ef414179173
Diego Yagüe <dyaguejuarez@gmail.com>
2015-02-06 15:24:58 +0100

    Added file.exists()

--- plot2.R
+++ plot2.R
@@ -5,34 +5,38 @@ working_directory <- getwd()
 file <- "household_power_consumption.txt"  
 path <- paste(working_directory, file, sep = "/") 
 
-# Read from local file
-population <- read.csv2(path, header = TRUE, sep = ";", na.strings = "?",
-                        colClasses=c("character","character","character",                        
-                                     "character","character","character",
-                                     "character","character"))
-
-#Transform the character values to a Date format. 
-population$Date<- as.Date(population$Date, "%d/%m/%Y")
-
-# Getting the subset data frame where Date = 2007-02-01 and 2007-02-02
-sample <- subset(population, Date == "2007-02-01" | Date == "2007-02-02")
-rm(population)
-
-#Add a column with the DateTime Values 
-sample$Datetime <- strptime(
-  x = paste(
-    sample$Date, sample$Time), 
-  format = "%Y-%m-%d %H:%M:%S")
-
-# Create plot and send to a file (no plot appears on screen)
-with(sample, plot(Datetime, Global_active_power, 
-                  ylab = "Global Active Power (Kilowatts)",
-                  xlab = "",
-                  type = "l",
-                  ))
-
-# Copy my plot to a PNG file in the working directory
-dev.copy(png, file = "plot2.png", width = 480, height = 480)
-
-## Close the PNG file device
-dev.off()
+if (!file.exists(path)) {
+  stop("The file is not available")
+} else {
+  # Read from local file
+  population <- read.csv2(path, header = TRUE, sep = ";", na.strings = "?",
+                          colClasses=c("character","character","character",                        
+                                       "character","character","character",
+                                       "character","character"))
+  
+  #Transform the character values to a Date format. 
+  population$Date<- as.Date(population$Date, "%d/%m/%Y")
+  
+  # Getting the subset data frame where Date = 2007-02-01 and 2007-02-02
+  sample <- subset(population, Date == "2007-02-01" | Date == "2007-02-02")
+  rm(population)
+  
+  #Add a column with the DateTime Values 
+  sample$Datetime <- strptime(
+    x = paste(
+      sample$Date, sample$Time), 
+    format = "%Y-%m-%d %H:%M:%S")
+  
+  # Create plot and send to a file (no plot appears on screen)
+  with(sample, plot(Datetime, Global_active_power, 
+                    ylab = "Global Active Power (Kilowatts)",
+                    xlab = "",
+                    type = "l",
+                    ))
+  
+  # Copy my plot to a PNG file in the working directory
+  dev.copy(png, file = "plot2.png", width = 480, height = 480)
+  
+  ## Close the PNG file device
+  dev.off()
+}
